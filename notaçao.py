a9f0ea69d684cb5921d19e3f9da72f7bde96db00
matheusmoreira <matheusmoreira27870863@gmail.com>
2024-02-20 18:41:38 -0300

    commit no pycharm

---
+++ notaçao.py
@@ -0,0 +1,42 @@
+#criar notação cientifica
+#a = 0.0000054
+#print('%.4e'%a)
+
+#notação cientifica
+#a=2.3421e+02
+#b=3.5154e+02
+#c = a/b
+#print('%.10e'%c)
+
+#modulo erro absoluto
+#a=abs(123450.10-123400)
+#print(a)
+
+#erro relativo
+#a = abs((123450.10-123400)/123450.10)
+#print(a)
+
+#erro relativo paramedir a area
+#a1=3.14*12**2
+#a2 = 3.14159265*12**2
+#ea = abs(a2-a1)
+#er = abs(a2-a1)/abs(a2)
+#print(ea)
+#print(er*100)
+#import numpy as np
+#v = np .array([[4,7,0,-8]])
+#u = np .array([[9,17,-12,21]])
+#m=v-u
+#print(bin(57))
+a= 1
+b = 28
+c= -10
+xv = b/(2*a)
+delta = b**2-4*a*c
+yv = -delta/(4*a)
+print(xv)
+print(yv)
+
+
+
+
---
+++ operações aritméticas.py
@@ -0,0 +1,9 @@
+# + adição
+# - subtração
+# * multiplicação
+# / divisão
+# ** potencia
+# // divisão inteira
+# % resto da divisão
+#print(81**(1/2))   rais quadrada
+print(81**(1/3))
